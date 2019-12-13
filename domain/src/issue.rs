ee5e67b096b4d58ddea6d3242158cbaa58045743
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2019-12-13 20:10:05 +0900

    途中

--- domain/src/issue.rs
+++ domain/src/issue.rs
@@ -1,8 +1,6 @@
 
 pub mod usecase;
 pub mod repository;
-pub use usecase::IssueUseCase;
-pub use usecase::create_issue_usecase;
 
 pub struct Issue {
     id: Option<IssueId>,
@@ -21,17 +19,3 @@ impl Issue {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::super::issue::*;
-    #[test]
-    fn test_create_issue() {
-        usecase::create_issue_usecase().create_issue(
-            &usecase::CreateIssue {
-                title: "title",
-                description: "description",
-                todos: vec!["todo1", "todo2"],
-                labels: vec!("bug", "feature")
-            });
-    }
-}    
--- domain/src/issue/usecase.rs
+++ domain/src/issue/usecase.rs
@@ -3,6 +3,8 @@
 use super::{Issue, IssueId};
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
+use super::super::{UseCase, UseCaseError};
+
 
 pub struct CreateIssue<'a> {
     pub title: &'a str,
@@ -11,14 +13,31 @@ pub struct CreateIssue<'a> {
     pub labels: Vec<&'a str>
 }
 
-pub fn create_issue_usecase() -> impl IssueUseCase {
+pub fn create_issue_usecase<'a>() -> impl UseCase<&'a CreateIssue<'a>, ()> {
     return IssueUseCaseInteractor {}
 }
 
 struct IssueUseCaseInteractor {}
 
-impl IssueUseCase for IssueUseCaseInteractor {
-    fn create_issue(&self, create_issue: &CreateIssue) {
+impl UseCase<&CreateIssue<'_>, ()> for IssueUseCaseInteractor {
+    fn exec(&self, create_issue: &CreateIssue) -> Result<(), UseCaseError>{
         Issue::new(create_issue.title, create_issue.labels.clone());
+        Ok(())
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_create_issue() {
+        let issue = CreateIssue {
+            title: "title",
+            description: "description",
+            todos: vec!["todo1", "todo2"],
+            labels: vec!("bug", "feature")
+        } ;
+        create_issue_usecase().exec(&issue);
     }
 }
